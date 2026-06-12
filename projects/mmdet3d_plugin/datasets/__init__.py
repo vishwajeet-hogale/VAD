a20dbbbbc4d5ef85dfe420ce36cc4446b4bd92bb
vishwajeet-hogale <vishwajeethogale307@gmail.com>
2026-06-12 10:49:56 -0700

    Guard NuScenes dataset import for OLV2 extraction

--- projects/mmdet3d_plugin/datasets/__init__.py
+++ projects/mmdet3d_plugin/datasets/__init__.py
@@ -1,8 +1,17 @@
-from .nuscenes_vad_dataset import VADCustomNuScenesDataset
 from .openlanev2_vad_dataset import VADOpenLaneV2Dataset
 
 
+try:
+    from .nuscenes_vad_dataset import VADCustomNuScenesDataset
+except ModuleNotFoundError as exc:
+    if exc.name != 'mmdet3d.ops.roiaware_pool3d':
+        raise
+    VADCustomNuScenesDataset = None
+
+
 __all__ = [
-    'VADCustomNuScenesDataset',
     'VADOpenLaneV2Dataset'
 ]
+
+if VADCustomNuScenesDataset is not None:
+    __all__.append('VADCustomNuScenesDataset')
